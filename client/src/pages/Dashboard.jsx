1aae253bab4670f033fe8d66eef1e6a63b6aea08
Dhiraj <royd3366@gmail.com>
2025-10-27 15:59:34 +0530

    Small Changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -144,10 +144,7 @@ const Dashboard = () => {
 
   return (
     <div>
-      <div className="min-w-7xl mx-auto px-4 py-8">
-        <p className="text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden">
-          Welcome, John Doe
-        </p>
+      <div className="min-w-7xl mx-auto px-4 py-8">       
         <div className="flex gap-4">
           <button
             onClick={() => setShowCreateResume(true)}
